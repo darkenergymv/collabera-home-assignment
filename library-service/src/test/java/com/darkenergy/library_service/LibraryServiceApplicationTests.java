96d0556feb481265db2a514c998e38893d7c1836
Dark Energy <mohamedmauroof.mv@gmail.com>
2025-08-13 03:49:02 +0800

    scaffold a minimalistic Spring Boot project

---
+++ library-service/src/test/java/com/darkenergy/library_service/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkenergy.library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
