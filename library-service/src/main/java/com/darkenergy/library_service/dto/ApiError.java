7077e7aedc017403b3a635e6c732528beb65ccfa
Dark Energy <mohamedmauroof.mv@gmail.com>
2025-08-15 04:34:18 +0800

    fixed Lombok annotation processing bug

--- library-service/src/main/java/com/darkenergy/library_service/dto/ApiError.java
+++ library-service/src/main/java/com/darkenergy/library_service/dto/ApiError.java
@@ -3,29 +3,28 @@ package com.darkenergy.library_service.dto;
 import java.time.Instant;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 public class ApiError {
-	@Getter @Setter 
 	private Instant timestamp = Instant.now();
-	
-	@Getter @Setter 
+
 	private int status;
-	
-	@Getter @Setter 
+
 	private String message;
-	
-	@Getter @Setter 
+
 	private List<String> details;
-	
-	public ApiError() {}
-	
+
+	public ApiError() {	}
+
 	public ApiError(int status, String message) {
-	    this.status = status; this.message = message;
-    }
-	
+		this.status = status;
+		this.message = message;
+	}
+
 	public ApiError(int status, String message, List<String> details) {
-	    this.status = status; this.message = message; this.details = details;
-	  }
+		this.status = status;
+		this.message = message;
+		this.details = details;
+	}
 }
--- library-service/src/main/java/com/darkenergy/library_service/model/Book.java
+++ library-service/src/main/java/com/darkenergy/library_service/model/Book.java
@@ -8,13 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
-import lombok.Builder;
+import lombok.Data;
 
 @Entity
 @Table(name = "books", indexes = {
 	    @Index(name = "idx_books_isbn", columnList = "isbn")
 	})
-@Builder
+@Data
 public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,46 +39,6 @@ public class Book {
 		this.isbn = isbn;
 		this.title = title;
 		this.author = author;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public Long getVersion() {
-		return version;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public void setVersion(Long version) {
-		this.version = version;
 	}
 
 }
